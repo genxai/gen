593dfdbfcc8afd686d46fd14daef60b64c026b90
Nikita <github@nikiv.dev>
2025-04-11 14:38:25 +0200

    .

--- app/(app)/components/JazzAndAuth.tsx
+++ app/(app)/components/JazzAndAuth.tsx
@@ -22,7 +22,7 @@ export function JazzAndAuth({ children }: { children: ReactNode }) {
     >
       <>
         <AuthProvider>{children}</AuthProvider>
-        <JazzDevTools />
+        {/* <JazzDevTools /> */}
       </>
     </JazzProvider>
   )
