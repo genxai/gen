654a29c2c73908e14e4febbdc39953cfa6f016fd
Nikita <github@nikiv.dev>
2025-04-17 16:29:39 +0200

    crud try with jazz

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,12 +1,31 @@
 "use client"
 
+import { useAccount } from "jazz-react"
+import { useAuth } from "../contexts/Auth"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import PromptComponent from "./components/PromptComponent"
 
 export default function Page() {
+  const cloudAuth = useAuth()
+  const { me } = useAccount({
+    resolve: {
+      profile: true,
+      root: true,
+    },
+  })
   return (
     <div className="bg-background text-foreground w-full h-screen">
+      {/* {cloudAuth.authClient && (
+        <div
+          onClick={() => {
+            console.log(me, "me")
+            // me?.root.tokens = me?.root.tokens + 10
+          }}
+        >
+          give 10 tokens to logged in user
+        </div>
+      )} */}
       <Navbar />
       <PromptComponent />
       <Footer />
